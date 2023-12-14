19412443dbe26e9919d4e41283bc8dbcab2b30a7
RoashiniV <153625531+RoashiniV@users.noreply.github.com>
2023-12-14 09:34:32 +0530

    Add files via upload

---
+++ day1/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+// import Login from './components/Login';
+import Reg from './components/Reg';
+function App() {
+  return (
+    <div className="App">
+      <Reg />
+    </div>
+  );
+}
+
+export default App;
